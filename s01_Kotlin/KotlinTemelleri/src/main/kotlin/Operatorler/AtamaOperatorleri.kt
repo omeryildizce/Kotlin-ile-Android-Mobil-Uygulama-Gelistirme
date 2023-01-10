40fbf57df3a6be187494c9ded0ec7da4083ee3ed
omeryildizce <omeryildizce@gmail.com>
2023-01-10 15:20:05 +0300

    Create AtamaOperatorleri.kt

---
+++ s01_Kotlin/KotlinTemelleri/src/main/kotlin/Operatorler/AtamaOperatorleri.kt
@@ -0,0 +1,19 @@
+package Operatorler
+
+fun main() {
+    var sayi = 5
+
+    // +=, -=, *=, /=, %=
+    println("Sayı = 5 için,")
+    sayi += 5
+    println("+= 5 = " + sayi)
+    sayi -= 5
+    println("-= 5 = " + sayi)
+    sayi *= 5
+    println("*= 5 = " + sayi)
+    sayi /= 5
+    println("/= 5 = " + sayi)
+    sayi %= 5
+    println("%= 5 = " + sayi)
+
+}
