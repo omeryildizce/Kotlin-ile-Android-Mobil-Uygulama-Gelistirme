66ae20415935cab43636e23459b0fd77d4488f67
omeryildizce <omeryildizce@gmail.com>
2023-01-10 15:20:02 +0300

    Create AritmetikOperatorler.kt

---
+++ s01_Kotlin/KotlinTemelleri/src/main/kotlin/Operatorler/AritmetikOperatorler.kt
@@ -0,0 +1,11 @@
+package Operatorler
+
+fun main() {
+    var sayi1: Int = 15
+    var sayi2 = 4
+    println("Toplam: ${sayi1 + sayi2}")
+    println("Fark: " + (sayi1 - sayi2))
+    println("Çarpım: ${sayi1 * sayi2}")
+    println("Çarpım: ${sayi1 / sayi2.toDouble()}")
+    println("Mod: ${sayi1 % sayi2}")
+}
