7cb718944d20ee94c2c9b1c3b821cc4bdccea129
omeryildizce <omeryildizce@gmail.com>
2023-01-10 21:51:20 +0300

    Create NullKavrami.kt

---
+++ s01_Kotlin/KotlinTemelleri/src/main/kotlin/Diziler/NullKavrami.kt
@@ -0,0 +1,17 @@
+package Diziler
+
+fun main() {
+    var isim : String? = null
+    println(isim)
+    println(isim?.length)
+    // println(isim!!.length)
+
+    var sayi : Int?= null
+    sayi = 5
+    println(sayi)
+    sayi = null
+    println(sayi)
+
+
+
+}
