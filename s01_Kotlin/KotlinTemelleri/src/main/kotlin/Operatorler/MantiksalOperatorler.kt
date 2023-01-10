cafe459e1e29ad9c1a38ad47b3cac1963cae2a4f
omeryildizce <omeryildizce@gmail.com>
2023-01-10 15:20:13 +0300

    Create MantiksalOperatorler.kt

---
+++ s01_Kotlin/KotlinTemelleri/src/main/kotlin/Operatorler/MantiksalOperatorler.kt
@@ -0,0 +1,38 @@
+package Operatorler
+
+fun main() {
+
+    println("Ve operatörü &&")
+    println(false && false)
+    println(false && true)
+    println(true && false)
+    println(true && true)
+
+
+    println()
+    println("Veya operatörü ||")
+    println(false || false)
+    println(false || true)
+    println(true || false)
+    println(true || true)
+
+    println()
+    println("Eşit operatörü ==")
+    println(false == false)
+    println(false == true)
+    println(true == false)
+    println(true == true)
+
+    println()
+
+    println("Eşit değil operatörü !=")
+    println(false != false)
+    println(false != true)
+    println(true != false)
+    println(true != true)
+
+    println("Eşit değil !")
+    println(!false)
+    println(!true)
+
+}
