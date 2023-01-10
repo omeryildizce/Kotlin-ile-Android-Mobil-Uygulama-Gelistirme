8cc46ab668dccce07c3dacb6954abf8252374958
omeryildizce <omeryildizce@gmail.com>
2023-01-10 14:25:37 +0300

    Create Number.kt

---
+++ s01_Kotlin/KotlinTemelleri/src/main/kotlin/Degiskenler/Number.kt
@@ -0,0 +1,45 @@
+package Degiskenler
+
+fun main() {
+    // Veri türleri
+    // Number
+    // byte, short, int, long, float, double
+
+    println("--Byte--")
+    println("min: " + Byte.MIN_VALUE)
+    println("max: " + Byte.MAX_VALUE)
+    println("bit: " + Byte.SIZE_BITS)
+    println("byte: " + Byte.SIZE_BYTES)
+
+    println("--Short--")
+    println("min: " + Short.MIN_VALUE)
+    println("max: " + Short.MAX_VALUE)
+    println("bit: " + Short.SIZE_BITS)
+    println("byte: " + Short.SIZE_BYTES)
+
+    println("--Int--")
+    println("min: " + Int.MIN_VALUE)
+    println("max: " + Int.MAX_VALUE)
+    println("bit: " + Int.SIZE_BITS)
+    println("byte: " + Int.SIZE_BYTES)
+
+    println("--Long--")
+    println("min: " + Long.MIN_VALUE)
+    println("max: " + Long.MAX_VALUE)
+    println("bit: " + Long.SIZE_BITS)
+    println("byte: " + Long.SIZE_BYTES)
+
+
+    println("--Float--")
+    println("min: " + Float.MIN_VALUE)
+    println("max: " + Float.MAX_VALUE)
+    println("bit: " + Float.SIZE_BITS)
+    println("byte: " + Float.SIZE_BYTES)
+
+
+    println("--Double--")
+    println("min: " + Double.MIN_VALUE)
+    println("max: " + Double.MAX_VALUE)
+    println("bit: " + Double.SIZE_BITS)
+    println("byte: " + Double.SIZE_BYTES)
+}
