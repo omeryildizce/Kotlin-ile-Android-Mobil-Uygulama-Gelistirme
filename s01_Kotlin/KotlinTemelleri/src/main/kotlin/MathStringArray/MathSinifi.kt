c7d2d2523ed658859e872371755d2dcfc7fecbaa
omeryildizce <omeryildizce@gmail.com>
2023-01-11 15:01:34 +0300

    Create MathSinifi.kt

---
+++ s01_Kotlin/KotlinTemelleri/src/main/kotlin/MathStringArray/MathSinifi.kt
@@ -0,0 +1,16 @@
+package MathStringArray
+
+fun main() {
+    println(Math.E)
+    println(Math.PI)
+    println(Math.E / Math.PI)
+    println(Math.abs(-9))
+    println(Math.ceil(1.1))
+    println(Math.floor(1.9))
+    println(Math.pow(2.0,5.0))
+    println(Math.sqrt(25.0))
+    println(Math.max(10,15))
+    println(Math.min(10,15))
+    println(Math.random() * 100)
+    println(Math.pow(5.0,2.0) / 2)
+}
