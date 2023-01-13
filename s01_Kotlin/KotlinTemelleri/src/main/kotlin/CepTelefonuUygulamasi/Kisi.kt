0f5a6992db953f094e64b2a4d5040c4ed44b8334
omeryildizce <omeryildizce@gmail.com>
2023-01-13 13:01:51 +0300

    Create Kisi.kt

---
+++ s01_Kotlin/KotlinTemelleri/src/main/kotlin/CepTelefonuUygulamasi/Kisi.kt
@@ -0,0 +1,10 @@
+package CepTelefonuUygulamasi
+
+class Kisi (var isim:String, var telNo: String){
+
+    companion object{
+        fun kisiOlustur(isim: String, telNo: String) : Kisi{
+            return Kisi(isim, telNo)
+        }
+    }
+}
