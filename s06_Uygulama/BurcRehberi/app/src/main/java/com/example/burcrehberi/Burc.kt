10e7b6d68c7e83a0bd4b18677ceb55d189c4b077
omeryildizce <omeryildizce@gmail.com>
2023-01-18 22:48:14 +0300

    Create Burc.kt

---
+++ s06_Uygulama/BurcRehberi/app/src/main/java/com/example/burcrehberi/Burc.kt
@@ -0,0 +1,3 @@
+package com.example.burcrehberi
+import java.io.Serializable
+data class Burc(var burcAdi: String, var burcTarih: String, var burcSembol: Int, var buyukBurcResim: Int, var burcGenelOzellikler:String) :  Serializable {}
