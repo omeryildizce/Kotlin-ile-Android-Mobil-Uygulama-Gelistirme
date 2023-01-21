de9a835484387ea67341aa58f5fd911c809be6c6
omeryildizce <omeryildizce@gmail.com>
2023-01-21 21:32:59 +0300

    Youtube Play List

---
+++ s09_Uygulama/YoutubePlaylist/app/src/main/java/com/example/youtubeplaylist/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.youtubeplaylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Retrofit
+
+class MainActivity : AppCompatActivity() {
+    val API_KEY = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
